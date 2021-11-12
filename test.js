d3ac4298e05578b32af476f665ccb8e36d923ac4
fujimon0211 <89776152+fujimon0211@users.noreply.github.com>
2021-11-12 22:28:25 +0900

    最終段階の調整終了

--- test.js
+++ test.js
@@ -1,3 +1,4 @@
+//一個目の写真の入れ替わり
 function imageChange() {
     document.getElementById("album").innerHTML = "<img src='img17.jpg'>";
 }
@@ -10,42 +11,46 @@ function imageMoreNew() {
 function imageReturn() {
     document.getElementById("album").innerHTML = "<img src='img14.jpg'>";
 }
+//２個目の写真の入れ替わり
 function imageChange2() {
     document.getElementById("album2").innerHTML = "<img src='img6.jpg'>";
 }
 function imageNew2() {
-    document.getElementById("album").innerHTML = "<img src='img19.jpg'>";
+    document.getElementById("album2").innerHTML = "<img src='img19.jpg'>";
 }
 function imageMoreNew2() {
-    document.getElementById("album").innerHTML = "<img src='img18.jpg'>";
+    document.getElementById("album2").innerHTML = "<img src='img18.jpg'>";
 }
 function imageReturn2() {
     document.getElementById("album2").innerHTML = "<img src='img11.jpg'>";
 }
+//3個目の写真の入れ替わり
 function imageChange3() {
     document.getElementById("album3").innerHTML = "<img src='img8.jpg'>";
 }
 function imageNew3() {
-    document.getElementById("album").innerHTML = "<img src='img16.jpg'>";
+    document.getElementById("album3").innerHTML = "<img src='img16.jpg'>";
 }
 function imageMoreNew3() {
-    document.getElementById("album").innerHTML = "<img src='img15.jpg'>";
+    document.getElementById("album3").innerHTML = "<img src='img15.jpg'>";
 }
 function imageReturn3() {
     document.getElementById("album3").innerHTML = "<img src='img3.jpg'>";
 }
+//4個目の写真の入れ替わり
 function imageChange4() {
     document.getElementById("album4").innerHTML = "<img src='img7.jpg'>";
 }
 function imageNew4() {
-    document.getElementById("album").innerHTML = "<img src='img13.jpg'>";
+    document.getElementById("album4").innerHTML = "<img src='img13.jpg'>";
 }
 function imageMoreNew4() {
-    document.getElementById("album").innerHTML = "<img src='img4.jpg'>";
+    document.getElementById("album4").innerHTML = "<img src='img4.jpg'>";
 }
 function imageReturn4() {
     document.getElementById("album4").innerHTML = "<img src='img10.jpg'>";
 }
+//実行
 setInterval(imageReturn, 30000), setInterval(imageChange, 7500), setInterval(imageNew, 15000), setInterval(imageMoreNew, 22500)
 setInterval(imageReturn2, 30000), setInterval(imageChange2, 7500), setInterval(imageNew2, 15000),setInterval(imageMoreNew2, 22500)
 setInterval(imageReturn3,30000), setInterval(imageChange3, 7500), setInterval(imageNew3, 15000),setInterval(imageMoreNew3, 22500)

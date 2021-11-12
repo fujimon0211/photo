91fe7e1ca2aa463058f1c3980c5165411c36055a
fujimon0211 <89776152+fujimon0211@users.noreply.github.com>
2021-11-12 22:21:56 +0900

    コードを簡略化しました

--- test.js
+++ test.js
@@ -1,11 +1,23 @@
 function imageChange() {
     document.getElementById("album").innerHTML = "<img src='img17.jpg'>";
 }
+function imageNew() {
+    document.getElementById("album").innerHTML = "<img src='img12.jpg'>";
+}
+function imageMoreNew() {
+    document.getElementById("album").innerHTML = "<img src='img2.jpg'>";
+}
 function imageReturn() {
     document.getElementById("album").innerHTML = "<img src='img14.jpg'>";
 }
 function imageChange2() {
     document.getElementById("album2").innerHTML = "<img src='img6.jpg'>";
+}
+function imageNew2() {
+    document.getElementById("album").innerHTML = "<img src='img19.jpg'>";
+}
+function imageMoreNew2() {
+    document.getElementById("album").innerHTML = "<img src='img18.jpg'>";
 }
 function imageReturn2() {
     document.getElementById("album2").innerHTML = "<img src='img11.jpg'>";
@@ -13,23 +25,32 @@ function imageReturn2() {
 function imageChange3() {
     document.getElementById("album3").innerHTML = "<img src='img8.jpg'>";
 }
+function imageNew3() {
+    document.getElementById("album").innerHTML = "<img src='img16.jpg'>";
+}
+function imageMoreNew3() {
+    document.getElementById("album").innerHTML = "<img src='img15.jpg'>";
+}
 function imageReturn3() {
     document.getElementById("album3").innerHTML = "<img src='img3.jpg'>";
 }
 function imageChange4() {
     document.getElementById("album4").innerHTML = "<img src='img7.jpg'>";
 }
+function imageNew4() {
+    document.getElementById("album").innerHTML = "<img src='img13.jpg'>";
+}
+function imageMoreNew4() {
+    document.getElementById("album").innerHTML = "<img src='img4.jpg'>";
+}
 function imageReturn4() {
     document.getElementById("album4").innerHTML = "<img src='img10.jpg'>";
 }
-setInterval(imageReturn,8000)
-setInterval(imageChange,5000)
-setInterval(imageReturn2,7000)
-setInterval(imageChange2,5000)
-setInterval(imageReturn3,8000)
-setInterval(imageChange3,6000)
-setInterval(imageReturn4,9000)
-setInterval(imageChange4,7000)
+setInterval(imageReturn, 30000), setInterval(imageChange, 7500), setInterval(imageNew, 15000), setInterval(imageMoreNew, 22500)
+setInterval(imageReturn2, 30000), setInterval(imageChange2, 7500), setInterval(imageNew2, 15000),setInterval(imageMoreNew2, 22500)
+setInterval(imageReturn3,30000), setInterval(imageChange3, 7500), setInterval(imageNew3, 15000),setInterval(imageMoreNew3, 22500)
+setInterval(imageReturn4,30000), setInterval(imageChange4, 7500), setInterval(imageNew4, 15000),setInterval(imageMoreNew4, 22500)
+
 
 
 
